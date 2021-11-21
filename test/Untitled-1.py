c6b7e1c4044646ec9864eb6b6f15ebcd3959409e
Rakxit121 <rakxit121@outlook.com>
2021-11-21 13:59:26 +0545

    second commit

---
+++ test/Untitled-1.py
@@ -0,0 +1,2 @@
+import keyword()
+print(keyboard.kwlist)
---
+++ test/keyword-1.py
@@ -0,0 +1,14 @@
+import keyword
+print(keyword.kwlist)
+print()
+print()
+a = "apple"
+b=123
+c="123abc"
+print(b, " ", c)
+print(a, b, c)
+print()
+print(id(a))
+print(id(b))
+print(id(c))
+# Reassigned variable a
